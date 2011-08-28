741d77edbbea448a8e50f71e8282c8a3d20e5b5a
Paulo Almeida <paulo@diasalmeida.com>
2011-08-29 00:10:16 +0100

    Added status and task list filter

--- taskmage.py
+++ taskmage.py
@@ -3,14 +3,14 @@ from datetime import datetime
 
 class TaskListCSV(object):
     def __init__(self):
-        self.tasks = self.read_tasks()
+        self.tasks = []
+        self.read_tasks()
 
     def add_task(self, task):
         self.tasks.append(task)
         return task
 
     def read_tasks(self):
-        tasks = []
         try:
             fh = open("tasks.csv")
         except IOError:
@@ -18,18 +18,32 @@ class TaskListCSV(object):
         else:
             for line in fh:
                 line = line.strip()
-                uid, summary, description, date = line.split('\t')
-                task = Task(uid, summary, description, date)
-                tasks.append(task)
-        return tasks
+                args = line.split('\t')
+                task = Task(*args) 
+                self.tasks.append(task)
 
+    def filter_tasks(self, **kwargs):
+        filtered_tasks = set()
+        for attr, values in kwargs.iteritems():
+            for task in self.tasks:
+                try:
+                    task_attr = getattr(task, attr)
+                except AttributeError:
+                    break
+                if task_attr in values:
+                    filtered_tasks.add(task)
+            self.tasks = list(filtered_tasks)
+        
+            
     def write_tasks(self):
         with open ("tasks.csv", "w") as fh:
             for task in self.tasks:
                 fh.write('\t'.join([task.uid, 
                                     task.summary, 
                                     task.description,
-                                    task.date]))
+                                    task.date,
+                                    task.status,
+                                    str(task.logged_time)]))
                 fh.write('\n')
         
 
@@ -39,6 +53,7 @@ class Task(object):
                  summary=None, 
                  description=None, 
                  date=None,
+                 status='needs-action',
                  logged_time=0):
         if not uid:
             self.date = datetime.now().isoformat()
@@ -48,6 +63,7 @@ class Task(object):
             self.date = date
         self.summary = summary
         self.description = description
+        self.status = status
         self.logged_time = logged_time
 
     def __repr__(self):
